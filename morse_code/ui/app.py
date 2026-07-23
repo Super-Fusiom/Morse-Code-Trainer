f98304912c5fc17091f5abaf22315a2e12084937
Paul <80376466+Super-Fusiom@users.noreply.github.com>
2026-07-23 17:40:35 +1200

    Feat: Modulerised UI and added center.

--- morse_code/ui/app.py
+++ morse_code/ui/app.py
@@ -1,13 +1,21 @@
 from textual import on
 from textual.app import App, ComposeResult
-from textual.widgets import Label, Button
-
+from textual.containers import Container
+from textual.widgets import Label, Button, Header, Footer
 
 class Trainer(App):
     CSS_PATH = "style.css"
+
+    def on_mount(self):
+        self.theme = "atom-one-dark"
+
     def compose(self) -> ComposeResult:
-        yield Label("Morse code test!", id="morse_code")
-        yield Button("EXIT!", id="exit")
+        yield Header()
+        yield Footer()
+        yield Container(
+            Label("Morse code test!", id="morse_code"),
+            Button("EXIT!", id="exit"), id="main"
+        )
 
     @on(Button.Pressed, "#exit")
     def submit_pressed(self) -> None:
