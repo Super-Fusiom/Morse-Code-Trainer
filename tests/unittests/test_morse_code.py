3dcf02107ad6e8a1a3693e9ae9574ec0a49e73ba
Paul <80376466+Super-Fusiom@users.noreply.github.com>
2026-07-24 12:38:28 +1200

    Feat: Added unit tests for conversion.

---
+++ tests/unittests/test_morse_code.py
@@ -0,0 +1,14 @@
+import pytest
+
+from morse_code.functions.morse import MORSE_CODE_TO_LETTER as ML, LETTER_TO_MORSE_CODE as LM
+
+def test_letter_conversion() -> None:
+    assert ML["-"] == "T"
+    assert ML["..."] == "S"
+    assert ML["--.."] == "Z"
+    assert ML[".--"] == "W"
+
+    assert ML["...."] + ML["."] + ML[".-.."] + ML[".-.."] + ML["---"] == "HELLO"
+
+def test_morse_code_conversion() -> None:
+    assert LM["A"]
