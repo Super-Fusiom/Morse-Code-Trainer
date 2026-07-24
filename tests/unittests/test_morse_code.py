2a6db3935701cf085fca2686499b33c4dd7cdca4
Paul <80376466+Super-Fusiom@users.noreply.github.com>
2026-07-24 12:52:30 +1200

    Feat: Remove unessarry import and added Morse to Letter test.

--- tests/unittests/test_morse_code.py
+++ tests/unittests/test_morse_code.py
@@ -1,5 +1,3 @@
-import pytest
-
 from morse_code.functions.morse import MORSE_CODE_TO_LETTER as ML, LETTER_TO_MORSE_CODE as LM
 
 def test_letter_conversion() -> None:
@@ -11,4 +9,11 @@ def test_letter_conversion() -> None:
     assert ML["...."] + ML["."] + ML[".-.."] + ML[".-.."] + ML["---"] == "HELLO"
 
 def test_morse_code_conversion() -> None:
-    assert LM["A"]
+    assert LM["A"] == ".-"
+    assert LM["E"] == "."
+    assert LM["O"] == "---"
+    assert LM["Q"] == "--.-"
+
+    message: str = ".-- --- .-. .-.. -..".replace(" ", "")
+
+    assert message == LM["W"] + LM["O"] + LM["R"] + LM["L"] + LM["D"]
