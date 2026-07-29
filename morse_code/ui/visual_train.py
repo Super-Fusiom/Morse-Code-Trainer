990cae57d1f5f475b829606ac9308d9228ca20c9
Paul <80376466+Super-Fusiom@users.noreply.github.com>
2026-07-30 11:35:07 +1200

    Feat: Handled input on encoding.

--- morse_code/ui/visual_train.py
+++ morse_code/ui/visual_train.py
@@ -1,8 +1,9 @@
 """Visual Mode for trainer"""
 from textual import on
 from textual.containers import Container
+from textual.reactive import reactive
 from textual.screen import Screen
-from textual.widgets import Button, Label
+from textual.widgets import Button, Label, Log
 
 
 class VisualWelcome(Screen):
@@ -19,3 +20,49 @@ class VisualWelcome(Screen):
             Button("Decode GO!", id="decode"),
             Button("Yea nah", id="main_menu_exit")
         )
+    @on(Button.Pressed, "#main_menu_exit")
+    def exit_pressed(self):
+        self.app.switch_screen("menu")
+
+    @on(Button.Pressed, "#encode")
+    def encode_pressed(self):
+        self.app.switch_screen("visual_encode")
+
+    @on(Button.Pressed, "#decode")
+    def decode_pressed(self):
+        self.app.switch_screen("visual_decode")
+
+class VisualEncode(Screen):
+
+    current_morse = reactive("")
+
+    def compose(self):
+        yield Container(
+            Label("Encode THIS!"),
+            Container(
+                Label("Current letter to encode:"),
+                Label("", id="encode_randomiser")
+            ),
+            Container(
+                Label("Current morse:"),
+                Label(id="encode_input")
+            )
+        )
+
+    def on_key(self, event):
+        if event.key == self.app.settings.controls.short_beep:
+            if len(self.current_morse) >= 5:
+                return
+            self.current_morse += "."
+            self.query_one("#encode_input").update(self.current_morse)
+
+        if event.key == self.app.settings.controls.long_beep:
+            if len(self.current_morse) >= 5:
+                return
+            self.current_morse += "-"
+            self.query_one("#encode_input").update(f"{self.current_morse}")
+
+
+
+class VisualDecode(Screen):
+    pass
